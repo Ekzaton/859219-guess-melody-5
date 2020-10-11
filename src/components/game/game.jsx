2afda713de2d231ba714d3e7bac4582174f503eb
Ekzaton <ekzaton@gmail.com>
2020-10-11 15:47:21 +0500

    Created 'game.jsx' component

---
+++ src/components/game/game.jsx
@@ -0,0 +1,13 @@
+import {PureComponent} from "react";
+
+class Game extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: 0,
+    };
+  }
+}
+
+export default Game;
