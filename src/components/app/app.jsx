c58b8e487a0b3336056500ca12601f400fca85a5
Ekzaton <ekzaton@gmail.com>
2020-10-30 22:43:20 +0500

    Removed routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -5,14 +5,11 @@ import {Switch, Route, BrowserRouter} from "react-router-dom";
 import FailTries from "../fail-tries/fail-tries";
 import Game from "../game/game";
 import Login from "../login/login";
-import QuestionArtist from "../question-artist/question-artist";
-import QuestionGenre from "../question-genre/question-genre";
 import ResultSuccess from "../result-success/result-success";
 import Welcome from "../welcome/welcome";
 
 const App = (props) => {
   const {errorsCount, questions} = props;
-  const [firstQuestion, secondQuestion] = questions;
 
   return (
     <BrowserRouter>
@@ -26,18 +23,6 @@ const App = (props) => {
             />
           )}
         />
-        <Route exact path="/dev-artist">
-          <QuestionArtist
-            question={secondQuestion}
-            onAnswer={() => {}}
-          />
-        </Route>
-        <Route exact path="/dev-genre">
-          <QuestionGenre
-            question={firstQuestion}
-            onAnswer={() => {}}
-          />
-        </Route>
         <Route exact path="/login">
           <Login/>
         </Route>
