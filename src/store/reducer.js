f9e1fb1cb7d579d6561aa276a45004624372e487
Ekzaton <ekzaton@gmail.com>
2020-11-06 01:08:46 +0500

    Added max mistakes validation

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,20 +1,32 @@
 import {ActionType} from "./action";
 
+import {MAX_MISTAKE_COUNT} from "../const";
 import {extend} from "../utils";
+
+import questions from "../mocks/questions";
 
 const initialState = {
   mistakes: 0,
   step: 0,
+  questions,
 };
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.INCREMENT_STEP:
+      let nextStep = state.step + action.payload;
+
       return extend(state, {
-        step: state.step + action.payload,
+        step: nextStep,
       });
 
     case ActionType.INCREMENT_MISTAKES:
+      const mistakes = state.mistakes + action.payload;
+
+      if (mistakes >= MAX_MISTAKE_COUNT) {
+        return extend({}, initialState);
+      }
+
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
