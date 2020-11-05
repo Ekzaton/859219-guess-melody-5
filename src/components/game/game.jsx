52ef6272b1c6daf4931b11b960ac0f4353f46aac
Ekzaton <ekzaton@gmail.com>
2020-11-06 01:03:03 +0500

    Added incrementMistake to 'game.jsx'

--- src/components/game/game.jsx
+++ src/components/game/game.jsx
@@ -68,8 +68,9 @@ const mapDispatchToProps = (dispatch) => ({
   resetGame() {
     dispatch(ActionCreator.resetGame());
   },
-  onUserAnswer() {
+  onUserAnswer(question, answer) {
     dispatch(ActionCreator.incrementStep());
+    dispatch(ActionCreator.incrementMistake(question, answer));
   },
 });
 
