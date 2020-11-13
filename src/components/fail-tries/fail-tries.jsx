c78b88090b684fa903b53f2bf19a017df8c9c529
Ekzaton <ekzaton@gmail.com>
2020-11-13 16:51:12 +0500

    Added props to 'fail-tries.jsx' component

--- src/components/fail-tries/fail-tries.jsx
+++ src/components/fail-tries/fail-tries.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
-const FailTries = () => {
+import {ActionCreator} from "../../store/action";
+
+const FailTries = (props) => {
+  const {onReplayButtonClick, resetGame} = props;
+
   return (
     <section className="result">
       <div className="result__logo">
@@ -8,9 +14,31 @@ const FailTries = () => {
       </div>
       <h2 className="result__title">Какая жалость!</h2>
       <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
-      <button className="replay" type="button">Попробовать ещё раз</button>
+      <button
+        onClick={() => {
+          resetGame();
+          onReplayButtonClick();
+        }}
+        className="replay"
+        type="button"
+      >
+        Попробовать ещё раз
+      </button>
     </section>
   );
 };
 
-export default FailTries;
+FailTries.propTypes = {
+  onReplayButtonClick: PropTypes.func.isRequired,
+  resetGame: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  resetGame() {
+    dispatch(ActionCreator.resetGame());
+  },
+});
+
+export {FailTries};
+
+export default connect(null, mapDispatchToProps)(FailTries);
