d6c24fb848f46d222752edc6611919656f9e7f4d
Ekzaton <ekzaton@gmail.com>
2020-11-19 16:55:10 +0500

    Connect data loading

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import thunk from "redux-thunk";
 
 import {createAPI} from "./services/api";
 
+import {fetchQuestionList} from "./store/api-action";
 import {reducer} from "./store/reducer";
 
 import App from "./components/app/app";
@@ -16,6 +17,8 @@ const store = createStore(
     reducer,
     applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(fetchQuestionList());
 
 ReactDOM.render(
     <Provider store={store}>
