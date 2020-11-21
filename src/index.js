ae609caa3c9d0eabdd0036e58eeafcefa0253046
Ekzaton <ekzaton@gmail.com>
2020-11-21 13:14:04 +0500

    Connected authorization

--- src/index.js
+++ src/index.js
@@ -6,23 +6,32 @@ import thunk from "redux-thunk";
 
 import {createAPI} from "./services/api";
 
-import {fetchQuestionList} from "./store/api-action";
 import rootReducer from "./store/reducers/root-reducer";
+import {requireAuthorization} from "./store/action";
+import {fetchQuestionList, checkAuth} from "./store/api-action";
+
+import {AuthorizationStatus} from "./const";
 
 import App from "./components/app/app";
 
-const api = createAPI();
+const api = createAPI(
+    () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(
     rootReducer,
     applyMiddleware(thunk.withExtraArgument(api))
 );
 
-store.dispatch(fetchQuestionList());
-
-ReactDOM.render(
-    <Provider store={store}>
-      <App/>
-    </Provider>,
-    document.querySelector(`#root`)
-);
+Promise.all([
+  store.dispatch(fetchQuestionList()),
+  store.dispatch(checkAuth()),
+])
+.then(() => {
+  ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      document.querySelector(`#root`)
+  );
+});
