0d8f705c6b10d1218a21cee62886aa06294729d7
Ekzaton <ekzaton@gmail.com>
2020-11-21 11:49:37 +0500

    Added api action for authorization

--- src/store/api-action.js
+++ src/store/api-action.js
@@ -1,6 +1,15 @@
-import {loadQuestions} from "./action";
+import {loadQuestions, requireAuthorization} from "./action";
+import {AuthorizationStatus} from "../const";
 
 export const fetchQuestionList = () => (dispatch, _getState, api) => (
   api.get(`/questions`)
     .then(({data}) => dispatch(loadQuestions(data)))
+);
+
+export const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch((err) => {
+      throw err;
+    })
 );
