a7e4e656f82a2ac2362a8080346e34b48006c02e
Ekzaton <ekzaton@gmail.com>
2020-11-25 09:19:31 +0500

    Added snapshot test for 'audio-player.jsx' component

---
+++ src/components/audio-player/audio-player.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import AudioPlayer from './audio-player';
+
+it(`AudioPlayer is rendered correctly`, () => {
+  const tree = renderer
+    .create(
+        <AudioPlayer
+          isPlaying={false}
+          isLoading={true}
+          onPlayButtonClick={() => {}}
+        >
+          <audio />
+        </AudioPlayer>,
+        {createNodeMock: () => {
+          return {};
+        }}
+    )
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
