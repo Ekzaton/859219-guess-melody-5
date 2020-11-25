edaa49b9789249e7b2f56b590b3029f90fe6f231
Ekzaton <ekzaton@gmail.com>
2020-11-25 12:41:35 +0500

    Fix 2

--- src/hocs/with-audio-player/with-audio-player.e2e.test.js
+++ src/hocs/with-audio-player/with-audio-player.e2e.test.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {configure, shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import withActivePlayer from "./with-audio-player";
+import withAudioPlayer from "./with-audio-player";
 
 configure({adapter: new Adapter()});
 
 const MockComponent = () => <div/>;
-const MockComponentWrapped = withActivePlayer(MockComponent);
+const MockComponentWrapped = withAudioPlayer(MockComponent);
 
-it.skip(`Should`, () => {
+it(`Should`, () => {
   const wrapper = shallow(<MockComponentWrapped/>);
-  expect(wrapper.state().activePlayerId).toEqual(-1);
+  expect(wrapper.state().activePlayerId).toEqual(0);
 });
